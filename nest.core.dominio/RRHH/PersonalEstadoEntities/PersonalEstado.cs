32257b7a8f506a4de365d93ba4807bb5a9231398
gabogth <gabogth@gmail.com>
2025-06-16 10:46:53 -0500

    Add PersonalEstado entity and related layers (#15)

--- nest.core.dominio/RRHH/PersonalEstadoEntities/PersonalEstado.cs
+++ nest.core.dominio/RRHH/PersonalEstadoEntities/PersonalEstado.cs
@@ -1,6 +1,9 @@
-﻿namespace nest.core.dominio.RRHH.PersonalEstadoEntities
+using nest.core.dominio.Security.Audit;
+using nest.core.dominio;
+
+namespace nest.core.dominio.RRHH.PersonalEstadoEntities
 {
-    public class PersonalEstado
+    public class PersonalEstado : IAuditable, IEntity<byte>
     {
         public byte Id { get; set; }
         public string Nombre { get; set; }
