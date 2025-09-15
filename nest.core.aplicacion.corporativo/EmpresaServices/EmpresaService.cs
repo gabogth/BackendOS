a4a104226b5b6410da29d34c8a7c8a300f3c87c6
gabogth <gabogth@gmail.com>
2025-09-15 14:35:22 -0500

    Add Empresa support to corporativo service (#25)

--- nest.core.aplicacion.corporativo/EmpresaServices/EmpresaService.cs
+++ nest.core.aplicacion.corporativo/EmpresaServices/EmpresaService.cs
@@ -1,10 +1,11 @@
 using nest.core.dominio.Corporativo.Empresa;
 
-namespace nest.core.aplicacion.security.Corporativo
+namespace nest.core.aplicacion.corporativo.EmpresaServices
 {
     public class EmpresaService
     {
         private readonly IEmpresaRepository repository;
+
         public EmpresaService(IEmpresaRepository repository)
         {
             this.repository = repository;
@@ -13,6 +14,9 @@ namespace nest.core.aplicacion.security.Corporativo
         public Task<List<Empresa>> ObtenerTodos() => repository.ObtenerTodos();
         public Task<List<Empresa>> ObtenerActivos() => repository.ObtenerActivos();
         public Task<Empresa?> ObtenerPorId(int id) => repository.ObtenerPorId(id);
+        public Task<Empresa> Agregar(EmpresaCrearDto entry) => repository.Agregar(entry);
+        public Task<Empresa> Modificar(int id, EmpresaCrearDto entry) => repository.Modificar(id, entry);
+        public Task Eliminar(int id) => repository.Eliminar(id);
     }
 }
 
--- nest.core.infraestructura.corporativo/Mapper/AutomapperProfiles.cs
+++ nest.core.infraestructura.corporativo/Mapper/AutomapperProfiles.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using nest.core.dominio.Corporativo.EstructuraOrganizacionalTipoEntities;
 using nest.core.dominio.Corporativo.EstructuraOrganizacionalEntities;
+using nest.core.dominio.Corporativo.Empresa;
 
 namespace nest.core.infraestructura.corporativo.Mapper
 {
@@ -10,6 +11,7 @@ namespace nest.core.infraestructura.corporativo.Mapper
         {
             CreateMap<EstructuraOrganizacionalTipoCrearDto, EstructuraOrganizacionalTipo>();
             CreateMap<EstructuraOrganizacionalCrearDto, EstructuraOrganizacional>();
+            CreateMap<EmpresaCrearDto, Empresa>();
         }
     }
 }
