3c350e40ee9460b3705aa126f2da409e48d3b7b6
gabogth <gabogth@gmail.com>
2025-09-30 15:48:29 -0500

    Add patrimonial Activo API layer

---
+++ nest.core.patrimonial/Extensions/ConfigureServices.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using nest.core.aplicacion.patrimonial;
+using nest.core.aplicacion.patrimonial.ActivoServices;
+using nest.core.dominio.Cache;
+using nest.core.infraestructura.db.Cache;
+
+namespace nest.core.patrimonial.Extensions
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection ConfigureAplication(this IServiceCollection services, IConfigurationManager configuration)
+        {
+            ConfigureCache(services, configuration);
+            services.ConfigureInfraestructura(configuration);
+            services.AddScoped<ActivoService>();
+            return services;
+        }
+
+        private static void ConfigureCache(IServiceCollection services, IConfigurationManager configuration)
+        {
+            bool useRedis = configuration.GetValue<bool>($"RedisConfig:Enabled");
+            if (useRedis)
+            {
+                services.AddStackExchangeRedisCache(options =>
+                {
+                    options.Configuration = configuration.GetValue<string>($"RedisConfig:ConnectionString");
+                    options.InstanceName = configuration.GetValue<string>($"RedisConfig:InstanceName");
+                });
+                services.AddScoped<ICacheRepository, RedisCacheRepository>();
+            }
+            else
+            {
+                services.AddMemoryCache();
+                services.AddScoped<ICacheRepository, MemoryCacheRepository>();
+            }
+        }
+    }
+}
