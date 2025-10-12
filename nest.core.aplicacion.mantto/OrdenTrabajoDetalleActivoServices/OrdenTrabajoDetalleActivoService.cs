693ee966fb4fe8a0bad7e79e301a69c92f8a54ed
GaboGTH <gabogth@gmail.com>
2025-10-12 14:00:24 -0500

    add master detail

--- nest.core.aplicacion.mantto/OrdenTrabajoDetalleActivoServices/OrdenTrabajoDetalleActivoService.cs
+++ nest.core.aplicacion.mantto/OrdenTrabajoDetalleActivoServices/OrdenTrabajoDetalleActivoService.cs
@@ -5,20 +5,14 @@ namespace nest.core.aplicacion.mantto.OrdenTrabajoDetalleActivoServices
     public class OrdenTrabajoDetalleActivoService
     {
         private readonly IOrdenTrabajoDetalleActivoRepository repository;
-
         public OrdenTrabajoDetalleActivoService(IOrdenTrabajoDetalleActivoRepository repository)
         {
             this.repository = repository;
         }
-
         public Task<OrdenTrabajoDetalleActivo> ObtenerPorId(long id) => repository.ObtenerPorId(id);
-
         public Task<List<OrdenTrabajoDetalleActivo>> ObtenerPorDetalle(long ordenTrabajoDetalleId) => repository.ObtenerPorDetalle(ordenTrabajoDetalleId);
-
         public Task<OrdenTrabajoDetalleActivo> Agregar(OrdenTrabajoDetalleActivoCrearDto entry) => repository.Agregar(entry);
-
         public Task<OrdenTrabajoDetalleActivo> Modificar(long id, OrdenTrabajoDetalleActivoCrearDto entry) => repository.Modificar(id, entry);
-
         public Task Eliminar(long id) => repository.Eliminar(id);
     }
 }
