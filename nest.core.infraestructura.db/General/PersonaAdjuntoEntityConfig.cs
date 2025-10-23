50065103e6487c21c5dd201cd4a2b37dd692e7b2
GaboGTH <gabogth@gmail.com>
2025-10-23 00:33:08 -0500

    add empresa

--- nest.core.infraestructura.db/General/PersonaAdjuntoEntityConfig.cs
+++ nest.core.infraestructura.db/General/PersonaAdjuntoEntityConfig.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using nest.core.dominio.General.PersonaAdjuntoEntities;
-using nest.core.infraestructura.db.Utils;
 
 namespace nest.core.infraestructura.db.General
 {
@@ -11,6 +10,7 @@ namespace nest.core.infraestructura.db.General
         {
             builder.ToTable("persona_adjunto", "dbo");
             builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.EmpresaId);
             builder.Property(x => x.Id)
                 .ValueGeneratedNever()
                 .HasValueGenerator<GenericValueGenerator<long>>();
