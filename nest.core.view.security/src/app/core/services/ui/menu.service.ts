e09888bcc42b9202b06a05b0b89198652662c3f9
gabogth <gabogth@gmail.com>
2026-04-29 09:15:54 -0500

    feat(security-view): integrate fontawesome icons in nav

--- nest.core.view.security/src/app/core/services/ui/menu.service.ts
+++ nest.core.view.security/src/app/core/services/ui/menu.service.ts
@@ -9,36 +9,44 @@ export class MenuService {
     return [
       {
         label: 'Administración',
+        icon: 'fa-solid fa-screwdriver-wrench',
         children: [
           {
             label: 'Usuarios',
+            icon: 'fa-solid fa-users',
             route: '/usuarios',
           },
           {
             label: 'Roles',
+            icon: 'fa-solid fa-user-shield',
             route: '/roles',
           },
           {
             label: 'Formularios',
+            icon: 'fa-solid fa-file-lines',
             route: '/formularios',
           },
         ],
       },
       {
         label: 'Seguridad',
+        icon: 'fa-solid fa-shield-halved',
         children: [
           {
             label: 'Auditoría',
+            icon: 'fa-solid fa-clipboard-check',
             route: '/',
           },
           {
             label: 'Bitácora',
+            icon: 'fa-solid fa-book',
             route: '/',
           },
         ],
       },
       {
         label: 'Reportes',
+        icon: 'fa-solid fa-chart-column',
         route: '/',
       },
     ];
